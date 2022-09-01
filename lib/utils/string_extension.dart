39a78758a0b1bf6a99f6c208a39b5df660de0742
Jack Allison <jack@cuapps.co.uk>
2022-09-01 23:40:15 +0100

    Fix Typo in string_extension

--- lib/utils/string_extension.dart
+++ lib/utils/string_extension.dart
@@ -1,13 +1,13 @@
 extension StringExtension on String {
-  String ofMaxLength(int maxLenth, {bool withoutLineBreaks = false}) {
+  String ofMaxLength(int maxLength, {bool withoutLineBreaks = false}) {
     var string = this;
     if (withoutLineBreaks) {
       string = string.split("\n").first;
     }
-    if (string.length < maxLenth) {
+    if (string.length < maxLength) {
       return string;
     } else {
-      return '${string.substring(0, maxLenth - 3)}...';
+      return '${string.substring(0, maxLength - 3)}...';
     }
   }
 }
